ec486fc82c680ce75c8bb2398887c8fb461fe704
ivoisbelongtous <ivoisbelongtous@gmail.com>
2014-06-08 09:32:57 +0100

    Initial Commit

---
+++ TestAndPlay/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TestAndPlay
+//
+//  Created by Ivo Murrell on 08/06/2014.
+//  Copyright (c) 2014 Ivo Murrell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
